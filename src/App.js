752ea68d18a1ed4090ecaeccc42495b5b2396427
Esra <esratosun171@gmail.com>
2023-05-29 21:59:56 +0300

    class yapısı

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import {
+import React ,{
+    Component,
     useState
 }from "react";
 
+/*
 const oldApp = () => {
     //const sonuc = document.getElementById("sonuc");
     //Nesneyi bu fanctionun içinde aşağıdaki şekidle almak zorundayız
@@ -57,6 +59,88 @@ const App = () => {
             fontSize: 50
             }}>+</button>           
     </div>;
+}  */
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+        }
+    }
+
+    artir = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+
+    azalt = () => {
+        this.setState({
+            counter: this.state.counter - 1
+        })
+    }
+
+    callEsra = () => {
+        alert("Merhabaaaaaaa");
+    }
+    
+    render() {
+        return <div>
+            <button
+                onClick={this.callEsra}
+                style={{
+                    border: "2px solid #444",
+                    fontFamily: "monospace",
+                    backgroundColor: "#444",
+                    padding: "15px 20px",
+                    borderRadius: 8,
+                    marginRight: 20,
+                    color: "#00c2a9",
+                    outline: "none",
+                }}
+            >
+                Merhaba Esra
+            </button>
+            <button
+                onClick={this.callEsra}
+                className="button-outline"
+                style={{
+                    backgroundColor: "transparent",
+                    fontFamily: "monospace",
+                    borderColor: "#444",
+                    borderStyle: "solid",
+                    padding: "15px 20px",
+                    borderRadius: 8,
+                    color: "#444",
+                    marginRight: 20,
+                    outline: "none",
+                    borderWidth: 2,
+                }}
+            >
+                Merhaba Esra
+                {
+                    this.state.counter > 2 ? " X" : null
+                }
+            </button>
+            <button onClick={this.azalt} style={{
+                fontSize: 50
+                }}>-</button>      
+            <div style={{
+                fontSize: 50
+            }}>
+                {this.state.counter}    
+            </div>      
+            <button onClick={this.artir} style={{
+                fontSize: 50
+                }}>+</button>           
+        </div>;
+        
+    }
 }
 
 export default App;
+
+//render => componet sınıfından gelir
+//class da return olmadığı için render function sayesinde
+//return edilir.
